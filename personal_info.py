c4d5c9e6abffd0f7d1531078fad9bd604aeb8dcd
clarencejeddmaas <clarencemaas74@gmail.com>
2024-11-19 20:12:03 +0800

    Open the file personal_information.txt in append mode

--- personal_info.py
+++ personal_info.py
@@ -6,11 +6,4 @@ while True:
     number = (input("Enter your phone number: "))
     email = input("Enter your email address: ")
 
-    with open("./personal_info.txt", "a") as file_handle:
-        file_handle.write("\nPersonal Information\n")
-        file_handle.write(f"Name: {full_name}\n")
-        file_handle.write(f"Age: {age}\n")
-        file_handle.write(f"Date of Birth: {birthday}\n")
-        file_handle.write(f"Address: {address}\n")
-        file_handle.write(f"Phone Number: {number}\n")
-        file_handle.write(f"E-mail address: {email}\n")
+    with open("./personal_information.txt", "a") as file_handle:
