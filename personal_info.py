2ac47f3a48c80ecbbb960ed6af80ac47cce78da1
clarencejeddmaas <clarencemaas74@gmail.com>
2024-11-19 20:08:03 +0800

    Ask user if it wants to input another entry or information

--- personal_info.py
+++ personal_info.py
@@ -6,11 +6,16 @@ while True:
     number = (input("Enter your phone number: "))
     email = input("Enter your email address: ")
 
-with open("./personal_info.txt", "a") as file_handle:
-    file_handle.write("\nPersonal Information\n")
-    file_handle.write(f"Name: {full_name}\n")
-    file_handle.write(f"Age: {age}\n")
-    file_handle.write(f"Date of Birth: {birthday}\n")
-    file_handle.write(f"Address: {address}\n")
-    file_handle.write(f"Phone Number: {number}\n")
-    file_handle.write(f"E-mail address: {email}\n")
+    with open("./personal_info.txt", "a") as file_handle:
+        file_handle.write("\nPersonal Information\n")
+        file_handle.write(f"Name: {full_name}\n")
+        file_handle.write(f"Age: {age}\n")
+        file_handle.write(f"Date of Birth: {birthday}\n")
+        file_handle.write(f"Address: {address}\n")
+        file_handle.write(f"Phone Number: {number}\n")
+        file_handle.write(f"E-mail address: {email}\n")
+
+        another_entry = input("Do you want another entry? (YES/NO): ")
+
+        if another_entry.lower() == 'no':
+            break
