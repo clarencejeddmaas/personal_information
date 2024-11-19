ca9c2157c764e04fec99c05303490f421464d8c7
clarencejeddmaas <clarencemaas74@gmail.com>
2024-11-19 19:29:48 +0800

    Asking user to input info

--- personal_info.py
+++ personal_info.py
@@ -3,4 +3,5 @@ with open("./personal_info.txt", "w") as file_handle:
     file_handle.write(input("Enter your age: "))
     file_handle.write(input("Enter your date of birth: "))
     file_handle.write(input("Enter your address: "))
-    file_handle.write(input("Enter your phone number: "))
+    file_handle.write(input("Enter your mobile number: "))
+    file_handle.write(input("Enter your e-mail address: "))
