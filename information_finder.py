6d7178cbf5741a98118396decaf2a987e726d92a
clarencejeddmaas <clarencemaas74@gmail.com>
2024-11-20 18:42:52 +0800

    Initialze a found flag

--- information_finder.py
+++ information_finder.py
@@ -1,5 +1,7 @@
 find_fullname = input("Enter a full name to search: ")
 
+found = False
+
 with open("./personal_information.txt", "r") as file_handle:
     lines = file_handle.readlines()
     for line in lines: 
