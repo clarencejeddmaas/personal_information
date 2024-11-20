e1e8a2dec3f034422507001fdc3f0a82e229f4a5
clarencejeddmaas <clarencemaas74@gmail.com>
2024-11-20 18:45:24 +0800

    Read the entire file content

--- information_finder.py
+++ information_finder.py
@@ -3,6 +3,7 @@ find_fullname = input("Enter a full name to search: ")
 found = False
 
 with open("./personal_information.txt", "r") as file_handle:
+    data = file_handle.read()
     lines = file_handle.readlines()
     for line in lines: 
         print(line.strip())
