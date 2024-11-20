6b5f56c245721abfe71fbe7b259cb7029c99983c
clarencejeddmaas <clarencemaas74@gmail.com>
2024-11-20 22:31:52 +0800

    Step-by-step procedure

--- information_finder.py
+++ information_finder.py
@@ -1,3 +1,16 @@
+# 1. Start with while loop to search multiple names.
+# 2. Initialize a found flag.
+# 3. Open the file in read mode.
+# 4. Read the entire file content.
+# 5. Split file content in double newline.
+# 6. Use for loop for each line.
+# 7. Check if the full name was found in the line.
+# 8. If the full name was in the line, print the information found.
+# 9. Update the found flag into True, and exit the loop.
+# 10. Print a message if no information was found.
+# 11. Ask the user if it wants to search another full name.
+# 12. If the user type 'no', the program will print an exit.
+
 while True:
     find_fullname = input("Enter a full name to search: ")
 
