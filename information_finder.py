c102c4c3a101a2dc821d01bad841ab491d538e4a
clarencejeddmaas <clarencemaas74@gmail.com>
2024-11-20 19:43:04 +0800

    Open file in read mode

--- information_finder.py
+++ information_finder.py
@@ -2,7 +2,7 @@ find_fullname = input("Enter a full name to search: ")
 
 found = False
 
-with open("./personal_information.txt", "r") as file:
+with open("./personal_information.txt", "r") as file_handle:
     data = file_handle.read()
     lines = data.split("\n\n")
     for line in lines:
